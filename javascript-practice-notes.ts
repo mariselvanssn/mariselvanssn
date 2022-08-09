5bba03d397379af82a3445671c2864ec34ac9f20
mariselvanssn <mariselvan.cse@gmail.com>
2022-08-09 15:24:20 +0530

    Update javascript-practice-notes.ts

--- javascript-practice-notes.ts
+++ javascript-practice-notes.ts
@@ -10,3 +10,16 @@ function myFunction(a, b) {
 myFunction(1,2) // Expected: 3
 myFunction(1,10) // Expected: 11
 myFunction(99,1) // Expected: 100
+
+// Comparison operators, strict equality:
+// Write a function that takes two values, say a and b, as arguments
+// Return true if the two values are equal and of the same type
+
+function myFunction(a, b) {
+ return a === b;
+}
+
+myFunction(2, 3)// Expected: false
+myFunction(3, 3) // Expected: true
+myFunction(1, '1') // Expected: false
+myFunction('10', '10') // Expected: true
