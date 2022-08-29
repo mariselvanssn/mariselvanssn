1870a57433b461c66832dc7215512421e8d50402
mariselvanssn <mariselvan.cse@gmail.com>
2022-08-29 13:45:48 +0530

    Update javascript-practice-notes.ts

--- javascript-practice-notes.ts
+++ javascript-practice-notes.ts
@@ -178,7 +178,21 @@
   myFunction(-4) // Expected: true
   myFunction(5) // Expected: false
   myFunction(-111) // Expected: false
-
+  
+  // Check if a number is a whole number
+  // Write a function that takes a number (a) as argument
+  // If a is a whole number (has no decimal place), return true
+  // Otherwise, return false
+   function myFunction(a) {
+     return Number.isInteger(a);
+   }
+  function myFunction(a) {
+   return a - Math.floor(a) === 0
+  }
+  myFunction(4) // Expected: true 
+  myFunction(1.123) // Expected: false
+  myFunction(1048) //Expected: true 
+  myFunction(10.48) // Expected: false 
 
 // 2.Javascript Arrays:
 
