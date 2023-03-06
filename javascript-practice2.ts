31437f0379f78d89238cd2754a688f2213234293
mariselvanssn <mariselvan.cse@gmail.com>
2023-03-06 21:50:00 +0530

    Create javascript-practice2.ts

---
+++ javascript-practice2.ts
@@ -0,0 +1,12 @@
+Javascript Basics:
+
+// Reassign a value to a variable
+// Assign a new value to the variable num. The code will not work the way it is. Find the mistake and fix it. Execute the corrected code.
+  let num = 1;
+  let num = 2;
+  console.log(num);
+
+// Output: 
+  let num = 1;
+  num = 2;
+  console.log(num);
