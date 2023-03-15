da6479312d8ea22933f2347c8fb351458851f772
mariselvanssn <mariselvan.cse@gmail.com>
2023-03-15 18:42:39 +0530

    Create programs.ts

---
+++ programs.ts
@@ -0,0 +1,27 @@
+
+// The factorial of a number is the product of all the numbers from 1 to that number. For example,
+
+// factorial of 5 is equal to 1 * 2 * 3 * 4 * 5 = 120.
+// program to find the factorial of a number
+
+// take input from the user
+const number = parseInt(prompt('Enter a positive integer: '));
+
+// checking if number is negative
+if (number < 0) {
+    console.log('Error! Factorial for negative number does not exist.');
+}
+
+// if number is 0
+else if (number === 0) {
+    console.log(`The factorial of ${number} is 1.`);
+}
+
+// if number is positive
+else {
+    let fact = 1;
+    for (i = 1; i <= number; i++) {
+        fact *= i;
+    }
+    console.log(`The factorial of ${number} is ${fact}.`);
+}
